2f0614b32f9087f9d888993dc51129eac22df348
Juckes <michaeljuckes@hotmail.com>
2024-07-08 21:14:11 +0100

    Update config.sh

--- azure_20_04/config.sh
+++ azure_20_04/config.sh
@@ -1,33 +1,12 @@
 #!/bin/bash
-
 APT_REPOSITORIES=(
   "main"
   "restricted"
   "universe"
   "multiverse"
-  "ppa:deadsnakes/ppa"
 )
 
 COMMON_PACKAGES=(
-  # "build-essential"
-  # "ca-certificates" newest version included already
-  # "lsb-release" newest version already included
-  # "software-properties-common" newest version already included - NOT on minimal though
-  # jq
-  # libasound2 newest version already included - NOT on minimal though
-  # libgbm-dev NOT on either version
-  # libgconf-2-4 NOT on either version
-  # libgtk2.0-0 NOT on either version
-  # libgtk-3-0 NOT on either version
-  # libNOTify-dev NOT on either version
-  # libnss3 newest version already included - NOT on minimal though
-  # libxss1 NOT on either version
-  # libxtst6 NOT on either version
-#   unzip NOT on either version
-#   xauth newest version already included - NOT on minimal though
-#   xvfb NOT on either version
-#   zip newest version already included
-#
   "build-essential"
   "jq"
   "libgbm-dev"
@@ -42,11 +21,13 @@ COMMON_PACKAGES=(
   "python3-pip"
 )
 
-
-
-
-# PYTHON_VERSION="3.7"
+DOCKER_COMPOSE_VERSION="1.29.2"
 
 TERRAFORM_VERSIONS=("1.7.3" "1.9.0")
 
-CHECKOV_VERSION="3.2.171"
+TERRAGRUNT_VERSION="0.55.1"
+
+CHECKOV_VERSION="2.2.94"
+
+NODE_VERSIONS=("20" "18" "16" "15" "14")
+DEFAULT_NODE_VERSION="16"
