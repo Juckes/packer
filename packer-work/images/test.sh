45145e08e937fa6b3672ee392d6b17535cc864ce
Michael <michaeljuckes@hotmail.com>
2024-09-05 15:40:26 +0100

    putting in terraform export variable value for the pipeline

--- packer-work/images/test.sh
+++ packer-work/images/test.sh
@@ -21,3 +21,6 @@ fi
 
 # Export the Terraform version as a pipeline variable
 echo "##vso[task.setvariable variable=TERRAFORM_VERSION]$TERRAFORM_VERSION"
+
+# Export Terraform version
+export TERRAFORM_VERSION="1.9.5-1"
