1b5d0dc3c666a8c6822f11edd7952c61f7197ccb
Michael <michaeljuckes@hotmail.com>
2024-09-23 16:13:55 +0100

    adding config to the packer build and pipeline

--- packer-work/images/tools.sh
+++ packer-work/images/tools.sh
@@ -106,14 +106,31 @@ done
 # Install common packages
 install_packages "${COMMON_PACKAGES[@]}"
 
+######################################
+# Remove any old versions of Docker
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
 
-# Docker Engine
-sudo apt-get install -y docker.io
+# Install necessary prerequisites
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+
+# Add Docker's official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Set up the stable Docker repository
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update the package index
+sudo apt-get update
+
+# Install Docker and containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+########################################
 sudo usermod -aG docker "$USER"
 newgrp docker
 
 sudo systemctl enable docker.service
-# sudo systemctl enable containerd.service
+sudo systemctl enable containerd.service
 
 # Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
