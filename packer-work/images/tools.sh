1e36a9deb8aec37b2aecacc68964bddbb6f6d42c
Michael <michaeljuckes@hotmail.com>
2024-09-24 14:31:36 +0100

    adding config to the packer build and pipeline

--- packer-work/images/tools.sh
+++ packer-work/images/tools.sh
@@ -53,43 +53,43 @@ for repo in "${APT_REPOSITORIES[@]}"; do
     sudo add-apt-repository -y "$repo"
 done
 
-# Remove existing Docker installations
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do
-    sudo apt-get remove -y $pkg
-done
+# # Remove existing Docker installations
+# for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do
+#     sudo apt-get remove -y $pkg
+# done
 
-# Add Docker's official GPG key:
-sudo apt-get install -y ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+# # Add Docker's official GPG key:
+# sudo apt-get install -y ca-certificates curl
+# sudo install -m 0755 -d /etc/apt/keyrings
+# sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+# sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the Docker repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# # Add the Docker repository to Apt sources:
+# echo \
+#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+#   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+#   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Install Docker packages
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# # Install Docker packages
+# sudo apt-get update
+# sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Re-enable man-db service after Docker installation
-sudo systemctl start man-db.service
+# # Re-enable man-db service after Docker installation
+# sudo systemctl start man-db.service
 
-# Add user to Docker group and start services
-sudo usermod -aG docker "$USER"
-newgrp docker
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
+# # Add user to Docker group and start services
+# sudo usermod -aG docker "$USER"
+# newgrp docker
+# sudo systemctl enable docker.service
+# sudo systemctl enable containerd.service
 
-# Restart services flagged by needrestart
-sudo systemctl restart --no-block dbus.service packagekit.service php8.1-fpm.service
+# # Restart services flagged by needrestart
+# sudo systemctl restart --no-block dbus.service packagekit.service php8.1-fpm.service
 
-# Install Docker Compose
-echo "Installing Docker Compose..."
-sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+# # Install Docker Compose
+# echo "Installing Docker Compose..."
+# sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
 
 # Install tfenv
 TFENV_DIR="/usr/local/tfenv"
