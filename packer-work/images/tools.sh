1dce945d1da81927546243f5b6da469d8bc1968f
Michael <michaeljuckes@hotmail.com>
2024-09-25 10:06:33 +0100

    adding config to the packer build and pipeline

--- packer-work/images/tools.sh
+++ packer-work/images/tools.sh
@@ -77,9 +77,6 @@ done
 # Install common packages
 install_packages "${COMMON_PACKAGES[@]}"
 
-# uninstall previous packages
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-
 # Docker Engine
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
